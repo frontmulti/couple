88e441e370c7bdfeda06e6b58afe2b2a5f7333f3
frontmulti <frontmulti@gmail.com>
2016-07-31 02:29:55 +0900

    setup, html5up-len and mongooes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  console.log('/');
+  fs.readFile(__dirname + '/html5up-lens/index.html', function(error, data) {
+    if(error) {
+      console.log(error);l
+    } else {
+      res.writeHead(200, {'Content-Type' : 'text/html'});
+      res.end(data);
+    }
+  });
 });
 
 module.exports = router;
